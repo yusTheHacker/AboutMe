e0436b5502bca493afc93177bcf237cb7d399d95
Yusley <5003489915@student.csn.edu>
2019-09-26 01:37:39 -0700

    Made the input edit text and the button invisible and the text appears but it cannot be edited. The keyboard is also taken away from the screen

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    lateinit var nicknameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+        editText = findViewById(R.id.nickname_edit)
+        nicknameTextView = findViewById(R.id.nickname_text)
+    }
+
+    private fun addNickname(view: View) {
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        //hiding keyboard
+
+        val inn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inn.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
